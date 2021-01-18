534ff5bcc1ee8242b2e15353d4a92bf6c80c6700
Juan Manuel Mougan <juanmougan@gmail.com>
2021-01-18 18:16:54 -0300

    Exists after downloading everything

--- main.ts
+++ main.ts
@@ -14,9 +14,9 @@ import { Job, Queue, QueueScheduler, Worker } from 'bullmq'
 
     // Add a few jobs to the queue
     const outDir = createOutputDirectory()
-    await addJobToQueue({ command: 'youtube-dl', args: ['--output', `${outDir}first.mp4`, 'https://www.youtube.com/watch?v=8XAHY3braUA'] }, queue)
-    await addJobToQueue({ command: 'youtube-dl', args: ['--output', `${outDir}second.mp4`, 'https://www.youtube.com/watch?v=S24KpEHXdEw'] }, queue)
-    await addJobToQueue({ command: 'youtube-dl', args: ['--output', `${outDir}third.mp4`, 'https://www.youtube.com/watch?v=RoPANT6bYAw'] }, queue)
+    await addJobToQueue({ command: 'youtube-dl', args: ['--output', `${outDir}/first`, '-f', '"bestvideo[ext=mp4]+bestaudio[ext=m4a]/best"', 'https://www.youtube.com/watch?v=8XAHY3braUA'] }, queue)
+    await addJobToQueue({ command: 'youtube-dl', args: ['--output', `${outDir}/second`, '-f', '"bestvideo[ext=mp4]+bestaudio[ext=m4a]/best"', 'https://www.youtube.com/watch?v=S24KpEHXdEw'] }, queue)
+    await addJobToQueue({ command: 'youtube-dl', args: ['--output', `${outDir}/third`, '-f', '"bestvideo[ext=mp4]+bestaudio[ext=m4a]/best"', 'https://www.youtube.com/watch?v=RoPANT6bYAw'] }, queue)
 
     // The docs say:
     // Jobs that get rate limited will actually end as delayed jobs, so you need at least one QueueScheduler 
@@ -29,6 +29,40 @@ import { Job, Queue, QueueScheduler, Worker } from 'bullmq'
       console.log("Processing job: ", job.id)
       return execProcess(job.data.command, job.data.args)
     })
+    
+    // Listen for results
+    let lastJob = false
+    worker.on("progress", async (job: Job, progress: number | object) => {
+      // Do something with the return value.
+      console.log("In progress: ", progress)
+      console.log("Jobs completed: ", await queue.getCompletedCount())
+      console.log("Jobs waiting to be processed: ", await queue.count())
+    })
+    worker.on("completed", async (job: Job, returnvalue: any) => {
+      // Do something with the return value.
+      console.log("Completed: ", returnvalue)
+      console.log("Jobs completed: ", await queue.getCompletedCount())
+      const jobsToBeProcessed = await queue.count()
+      console.log("Jobs waiting to be processed: ", jobsToBeProcessed)
+
+      // If there are no more jobs, exit OK
+      // TODO this is quite hacky, improve
+      if (jobsToBeProcessed === 0) {
+        if (lastJob) {
+          // I've finished processing, exit
+          process.exit(0)
+        } else {
+          // Mark that the next sucessful job should be the last one
+          lastJob = true
+        }
+      }
+    })
+    worker.on("failed", async (job: Job, failedReason: string) => {
+      // Do something with the return value.
+      console.error("Failed: ", failedReason)
+      console.log("Jobs completed: ", await queue.getCompletedCount())
+      console.log("Jobs waiting to be processed: ", await queue.count())
+    })
   }
 )()
 
@@ -37,10 +71,10 @@ async function verifyRedis() {
   exec('redis-cli ping', (error, stdout, stderr) => {
     if (error || stderr) {
       console.error("Something went wrong with Redis")
-      process.exit(1);
+      process.exit(1)
     } else if (stdout && !stdout.toString().includes("PONG")) {
       console.error("PONG not received from Redis")
-      process.exit(2);
+      process.exit(2)
     }
   })
 }
@@ -49,7 +83,7 @@ function createOutputDirectory() {
   const fs = require('fs')
   const outputDirName = `output/${Date.now()}`
   if (!fs.existsSync(outputDirName)){
-    fs.mkdirSync(outputDirName, { recursive: true });
+    fs.mkdirSync(outputDirName, { recursive: true })
   }
   return outputDirName
 }
